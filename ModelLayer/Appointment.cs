ae30a5a6fa4b117f1821c4335b5d5a3ac4fbf7c5
Rasmus Juhl <rasmusjuhl@gmail.com>
2015-11-13 10:19:22 +0100

    Added and seller reference to Appointment

--- ModelLayer/Appointment.cs
+++ ModelLayer/Appointment.cs
@@ -14,5 +14,7 @@ namespace ModelLayer
         public string Category { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
+        public Seller Seller { get; set; }
+        public Buyer Buyer { get; set; }
     }
 }
