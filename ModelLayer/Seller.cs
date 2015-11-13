7a4009f9677dd59ce586b476a822b3c740fa6460
Serec <soren.kaae@gmail.com>
2015-11-13 10:32:39 +0100

    added property to seller

--- ModelLayer/Seller.cs
+++ ModelLayer/Seller.cs
@@ -9,10 +9,12 @@ namespace ModelLayer
     public class Seller : Person
     {
         public int Id { get; set; }
-        public Seller(int id, string name, string address, Location location, string phone, string mobil, string email, string misc)
+        public List<Property> Properties { get; set; }
+        public Seller(int id, List<Property> properties, string name, string address, Location location, string phone, string mobil, string email, string misc)
             : base(name, address, location, phone, mobil, email, misc)
         {
             this.Id = id;
+            this.Properties = properties;
         }
 
     }
