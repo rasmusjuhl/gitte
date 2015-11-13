a491fe41ecb333d032d0b875c883afd3323e12d9
Rasmus Juhl <rasmusjuhl@gmail.com>
2015-11-13 09:58:24 +0100

    Added Apointment Class

---
+++ ModelLayer/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class Appointment
+    {
+        public DateTime Date { get; set; }
+        public DateTime StarTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
