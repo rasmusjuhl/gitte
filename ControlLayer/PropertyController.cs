ddc92de69899db636562f66183fc602a739dc17e
Serec <soren.kaae@gmail.com>
2015-11-17 13:31:19 +0100

    Added PropertyCoontroller

---
+++ ControlLayer/PropertyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer;
+using ModelLayer.DAL;
+
+namespace ControlLayer
+{
+    public class PropertyController
+    {
+        public PropertyController()
+        {
+
+        }
+
+        public void InsertProperty(Property property)
+        {
+            using (var ctx = new SystemContext())
+            {
+                ctx.Properties.Add(property);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
