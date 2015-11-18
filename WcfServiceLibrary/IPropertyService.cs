0ad4cfc98b6e399858048179b1aab050c586c26e
Serec <soren.kaae@gmail.com>
2015-11-18 13:47:38 +0100

    made interface public

--- WcfServiceLibrary/IPropertyService.cs
+++ WcfServiceLibrary/IPropertyService.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using ModelLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WcfServiceLibrary
 {
-    interface IPropertyService
+    [ServiceContract]
+    public interface IPropertyService
     {
+        [OperationContract]
+        void InsertProperty(Property property);
+        [OperationContract]
+        Property GetPropertyByAddress(string address);
+        [OperationContract]
+        List<Property> GetAllProperties();
+        [OperationContract]
+        void UpdateProperty(Property property, string address, Location location, string type, int rooms, int floors, double price, double propertySize, double houseSize, int constructionYear);
+        [OperationContract]
+        void DeleteProperty(Property property);
     }
 }
