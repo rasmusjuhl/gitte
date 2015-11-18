25598a2f45512e890885d53984567efa529fc684
Kasper <kapao92@gmail.com>
2015-11-18 15:13:44 +0100

    AppointmentController
    Appointment Controller added

---
+++ ControlLayer/AppointmentController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ModelLayer;
+using ModelLayer.DAL;
+
+namespace ControlLayer
+{
+    class AppointmentController
+    {
+        public AppointmentController()
+        {
+        
+        }
+
+        public void InsertAppointment(Appointment appointment)
+        {
+            using (var ctx = new SystemContext())
+            {
+                ctx.Appointments.Add(appointment);
+                ctx.SaveChanges();
+            }
+        }
+
+        public Appointment GetAppointment(DateTime date)
+        {
+            Appointment appointment;
+            using (var ctx = new SystemContext())
+            {
+                appointment = ctx.Appointments.Where(a => a.Date == date).Single();
+            }
+            return appointment;
+        }
+
+        public List<Appointment> GetAllAppointments()
+        {
+            using (var ctx = new SystemContext())
+            {
+                return ctx.Appointments.ToList();
+            }
+        }
+
+        public void UpdateAppointment(Appointment appointment, DateTime date, DateTime StartTime, DateTime EndTime, 
+            string category, string descricption, string status, Seller seller, Buyer buyer)
+        {
+            appointment.Date = date;
+            appointment.StarTime = StartTime;
+            appointment.EndTime = EndTime;
+            appointment.Category = category;
+            appointment.Description = descricption;
+            appointment.Status = status;
+            appointment.Seller = seller;
+            appointment.Buyer = buyer;
+
+            using (var ctx = new SystemContext())
+            {
+                ctx.Entry(appointment).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+            }
+        }
+
+        public void DeleteAppointment(Appointment appointment)
+        {
+            using (var ctx = new SystemContext())
+            {
+                ctx.Appointments.Attach(appointment);
+                ctx.Appointments.Remove(appointment);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
--- ControlLayer/Program.cs
+++ ControlLayer/Program.cs
@@ -11,12 +11,12 @@ namespace ControlLayer
     public class Program
     {
         private static LocationController lc = new LocationController();
+        private static AppointmentController ac = new AppointmentController();
         static void Main(string[] args)
         {
             var db = new SystemContext();
             Console.WriteLine(db.Database.Connection.ConnectionString);
             Console.ReadLine();
-
         }
     }
 }
