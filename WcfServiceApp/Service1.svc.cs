e7f21db975568e27290b6bcdee7d57ecf3ee7e8d
Serec <soren.kaae@gmail.com>
2015-11-18 10:38:49 +0100

    added wcf cloud service

--- WcfServiceApp/Service1.svc.cs
+++ WcfServiceApp/Service1.svc.cs
@@ -17,17 +17,6 @@ namespace WcfServiceApp
             return string.Format("You entered: {0}", value);
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
+        
     }
 }
