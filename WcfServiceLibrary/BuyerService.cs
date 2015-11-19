4e38bd62b46a2aa24bb03385dc0f1947af5dc564
Serec <soren.kaae@gmail.com>
2015-11-19 09:31:04 +0100

    added BuyerService + interface

---
+++ WcfServiceLibrary/BuyerService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer;
+using ControlLayer;
+
+namespace WcfServiceLibrary
+{
+    public class BuyerService : IBuyerService
+    {
+        static BuyerController bCtr = new BuyerController();
+        public void InsertBuyer(Buyer buyer)
+        {
+            bCtr.InsertBuyer(buyer);
+        }
+
+        public Buyer GetBuyerByPhone(string phone)
+        {
+            return bCtr.GetBuyerByPhone(phone);
+        }
+
+        public List<Buyer> GetAllBuyers()
+        {
+            return bCtr.GetAllBuyers();
+        }
+
+        public void UpdateBuyer(Buyer buyer, List<Property> properties, string name, string address, Location location, string phone, string mobil, string email, string misc, string estateType, double minPrice, double maxPrice, double lotSizeMin, double lotSizeMax, double probertySizeMin, double probertySizeMax, double desiredRoomsMin, double desiredRoomsMax, List<Location> desiredLocations, string otherPref, bool contactAllowedByBoligOne, bool contactAllowedByReal, bool allowedEmailSpam, bool inRKI, bool buyerApproved, string bank, bool ownesHouse, bool livesForRent)
+        {
+            bCtr.UpdateBuyer(buyer, properties, name, address, location, phone, mobil, email, misc, estateType, minPrice, maxPrice, lotSizeMin, lotSizeMax, probertySizeMin, probertySizeMax, desiredRoomsMin, desiredRoomsMax, desiredLocations, otherPref, contactAllowedByBoligOne, contactAllowedByReal, allowedEmailSpam, inRKI, buyerApproved, bank, ownesHouse, livesForRent);
+        }
+
+        public void DeleteBuyer(Buyer buyer)
+        {
+            bCtr.DeleteBuyer(buyer);
+        }
+    }
+}
---
+++ WcfServiceLibrary/IBuyerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer;
+
+namespace WcfServiceLibrary
+{
+    [ServiceContract]
+    public interface IBuyerService
+    {
+        [OperationContract]
+        void InsertBuyer(Buyer buyer);
+        [OperationContract]
+        Buyer GetBuyerByPhone(string phone);
+        [OperationContract]
+        List<Buyer> GetAllBuyers();
+        [OperationContract]
+        void UpdateBuyer(Buyer buyer, List<Property> properties, string name, string address, Location location, string phone, string mobil, string email, string misc, string estateType, double minPrice, double maxPrice, double lotSizeMin, double lotSizeMax, double probertySizeMin, double probertySizeMax, double desiredRoomsMin, double desiredRoomsMax, List<Location> desiredLocations, string otherPref, Boolean contactAllowedByBoligOne, Boolean contactAllowedByReal, Boolean allowedEmailSpam, Boolean inRKI, Boolean buyerApproved, string bank, Boolean ownesHouse, Boolean livesForRent);
+        [OperationContract]
+        void DeleteBuyer(Buyer buyer);
+    }
+}
