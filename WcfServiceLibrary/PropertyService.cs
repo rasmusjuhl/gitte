5534934ab3dedaef07884946f7cad18738354233
Serec <soren.kaae@gmail.com>
2015-11-19 10:22:40 +0100

    made services as WCF services

--- WcfServiceLibrary/PropertyService.cs
+++ WcfServiceLibrary/PropertyService.cs
@@ -3,8 +3,9 @@ using ModelLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace WcfServiceLibrary
 {
