19765df7d54bd6ff403f268656337805d26f54b7
SilasJeppe <Silas_Jeppe@hotmail.com>
2015-12-07 15:26:56 +0100

    Editted error in propertyTest + added appointmentTest
    Tests added.

---
+++ ControlLayerTest/AppointmentTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLayer;
+using ModelLayer.DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ControlLayerTest
+{
+    [TestClass]
+    public class AppointmentTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+        }
+        
+        [TestMethod]
+        public void CreateAndReadTest()
+        {
+            using (var context = new SystemContext())
+            {
+                //Creating properties for the Appointment
+                DateTime date = DateTime.Today;
+                DateTime meetingStart = DateTime.Now;
+                DateTime meetingEnd = DateTime.Now.AddHours(2);
+                //Create the Appointment itself 
+                Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1};
+                //Save it to the database
+                context.Appointments.Add(testAppointment);
+                context.SaveChanges();
+
+                //Compare Properties. Maybe use conditions other than Adress?
+                Appointment testAppointmentDB = context.Appointments.Where(x => x.Description == "Test for at finde en appointment").Single();
+                Assert.AreEqual<Appointment>(testAppointment, testAppointmentDB);
+                context.Appointments.Remove(testAppointment);
+                context.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            using (var context = new SystemContext())
+            {
+                ///Creating properties for the Appointment
+                DateTime date = DateTime.Today;
+                DateTime meetingStart = DateTime.Now;
+                DateTime meetingEnd = DateTime.Now.AddHours(2);
+                //Create the Appointment itself 
+                Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1 };
+                //Save it to the database
+                context.Appointments.Add(testAppointment);
+                context.SaveChanges();
+                
+                //Change the Appointment:
+                Appointment testAppointmentDB = context.Appointments.Where(x => x.Description == "Test for at finde en appointment").Single();
+                testAppointment.Description = "Ny beskrivelse";
+                context.SaveChanges();
+                
+                //Test the assertion
+                Assert.AreNotEqual(testAppointmentDB.Description, testAppointment.Description);
+                context.Appointments.Remove(testAppointment);
+                context.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            using (var context = new SystemContext())
+            {
+                ///Creating properties for the Appointment
+                DateTime date = DateTime.Today;
+                DateTime meetingStart = DateTime.Now;
+                DateTime meetingEnd = DateTime.Now.AddHours(2);
+                //Create the Appointment itself 
+                Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1 };
+                //Save it to the database
+                context.Appointments.Add(testAppointment);
+                context.SaveChanges();
+
+                //Delete it again
+                context.Appointments.Remove(testAppointment);
+                context.SaveChanges();
+
+                //Try to retrieve the deleted Appointment again, and assert it is null.
+                Appointment testAppointmentDB = context.Appointments.Where(x => x.Description == "Test for at finde en appointment").SingleOrDefault();
+                Assert.AreEqual(null, testAppointmentDB);
+            }
+        }
+    }
+}
--- ControlLayerTest/propertyTest.cs
+++ ControlLayerTest/propertyTest.cs
@@ -15,7 +15,7 @@ namespace ControlLayerTest
         public void Initialize()
         {
         }
-        
+
         [TestMethod]
         public void CreateAndReadTest()
         {
@@ -34,6 +34,54 @@ namespace ControlLayerTest
                 context.Properties.Remove(testProperty);
                 context.SaveChanges();
             }
+
         }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            using (var context = new SystemContext())
+            {
+                //No properties does need to be created before the property itself 
+                //Create the property itself 
+                Property testProperty = new Property() { Address = "Sofiendalshej 60", ZipCode = "9000", Type = "Borg", Rooms = 4, Floors = 1, Price = 1000000, PropertySize = 1000, HouseSize = 100, ConstructionYear = 2000, SellerID = 1 };
+                //Save it to the database
+                context.Properties.Add(testProperty);
+                context.SaveChanges();
+                
+                //Change the property:
+                Property testPropertyDB = context.Properties.Where(x => x.Address == "Sofiendalshej 60").Single();
+                testProperty.Address = "Her";
+                context.SaveChanges();
+                
+                //Test the assertion
+                Assert.AreNotEqual(testPropertyDB.Address, testProperty.Address);
+                context.Properties.Remove(testProperty);
+                context.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            using (var context = new SystemContext())
+            {
+                //No properties does need to be created before the property itself 
+                //Create the property itself 
+                Property testProperty = new Property() { Address = "Sofiendalshej 60", ZipCode = "9000", Type = "Borg", Rooms = 4, Floors = 1, Price = 1000000, PropertySize = 1000, HouseSize = 100, ConstructionYear = 2000, SellerID = 1 };
+                //Save it to the database
+                context.Properties.Add(testProperty);
+                context.SaveChanges();
+
+                //Delete it again
+                context.Properties.Remove(testProperty);
+                context.SaveChanges();
+
+                //Try to retrieve the deleted property again, and assert it is null.
+                Property testPropertyDB = context.Properties.Where(x => x.Address == "Sofiendalshej 60").SingleOrDefault();
+                Assert.AreEqual(null, testPropertyDB);
+            }
+        }
+
     }
 }
