d27cbdb2b94f20faf3f304a9541506db925d9f80
SilasJeppe <Silas_Jeppe@hotmail.com>
2015-12-08 12:10:42 +0100

    AppointmentTest almost done
    ServiceLayer refactoring requried (ListReturn-type)

--- ControlLayerTest/AppointmentTest.cs
+++ ControlLayerTest/AppointmentTest.cs
@@ -5,90 +5,103 @@ using ModelLayer.DAL;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ControlLayer;
 
 namespace ControlLayerTest
 {
     [TestClass]
     public class AppointmentTest
     {
+        private AppointmentController aCtr;
         [TestInitialize]
         public void Initialize()
         {
+            aCtr = new AppointmentController();
         }
-        
-        [TestMethod]
-        public void CreateAndReadTest()
-        {
-            using (var context = new SystemContext())
-            {
-                //Creating properties for the Appointment
-                DateTime date = DateTime.Today;
-                DateTime meetingStart = DateTime.Now;
-                DateTime meetingEnd = DateTime.Now.AddHours(2);
-                //Create the Appointment itself 
-                Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1};
-                //Save it to the database
-                context.Appointments.Add(testAppointment);
-                context.SaveChanges();
 
-                //Compare Properties. Maybe use conditions other than Adress?
-                Appointment testAppointmentDB = context.Appointments.Where(x => x.Description == "Test for at finde en appointment").Single();
-                Assert.AreEqual<Appointment>(testAppointment, testAppointmentDB);
-                context.Appointments.Remove(testAppointment);
-                context.SaveChanges();
-            }
+        [TestMethod]
+        public void CreateTest()
+        {
+            //Creating properties for the Appointment
+            DateTime date = DateTime.Today;
+            DateTime meetingStart = DateTime.Now;
+            DateTime meetingEnd = DateTime.Now.AddHours(2);
+            
+            //Create the Appointment itself 
+            Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1 };
+
+            //Save it to the database
+            int CountDB = aCtr.GetAllAppointments().ToList().Count;
+            aCtr.InsertAppointment(testAppointment);
+            int CountDBAfter = aCtr.GetAllAppointments().ToList().Count;
+
+            //Compare Properties. Maybe use conditions other than Adress?
+            Assert.AreEqual<int>(CountDBAfter, CountDB + 1);
+            aCtr.DeleteAppointment(testAppointment); ;
+        }
+
+        [TestMethod]
+        public void ReadTest()
+        {
+            //Creating properties for the Appointment
+            DateTime date = DateTime.Today;
+            DateTime meetingStart = DateTime.Now;
+            DateTime meetingEnd = DateTime.Now.AddHours(2);
+            
+            //Create the Appointment itself 
+            Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1 };
+
+            //Save it to the database
+            aCtr.InsertAppointment(testAppointment);
+
+            //Get property from database and assert not null
+            Assert.IsNotNull(aCtr.GetAppointment(date));
+            aCtr.DeleteAppointment(testAppointment);
         }
 
         [TestMethod]
         public void UpdateTest()
         {
-            using (var context = new SystemContext())
-            {
-                ///Creating properties for the Appointment
-                DateTime date = DateTime.Today;
-                DateTime meetingStart = DateTime.Now;
-                DateTime meetingEnd = DateTime.Now.AddHours(2);
-                //Create the Appointment itself 
-                Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1 };
-                //Save it to the database
-                context.Appointments.Add(testAppointment);
-                context.SaveChanges();
-                
-                //Change the Appointment:
-                Appointment testAppointmentDB = context.Appointments.Where(x => x.Description == "Test for at finde en appointment").Single();
-                testAppointment.Description = "Ny beskrivelse";
-                context.SaveChanges();
-                
-                //Test the assertion
-                Assert.AreNotEqual(testAppointmentDB.Description, testAppointment.Description);
-                context.Appointments.Remove(testAppointment);
-                context.SaveChanges();
-            }
+            //Creating properties for the Appointment
+            DateTime date = DateTime.Today;
+            DateTime meetingStart = DateTime.Now;
+            DateTime meetingEnd = DateTime.Now.AddHours(2);
+            
+            //Create the Appointment itself 
+            Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1 };
+
+            //Save it to the database
+            aCtr.InsertAppointment(testAppointment);
+
+            //Change the Appointment:
+
+            aCtr.UpdateAppointment(testAppointment, date, meetingStart, meetingEnd, "Ny", "Hej", "done", null, null);
+
+            //Test the assertion
+            Assert.AreNotEqual<string>("Sælgermøde", aCtr.GetAppointment(date).First().Category);
+            aCtr.DeleteAppointment(testAppointment);
         }
 
         [TestMethod]
         public void DeleteTest()
         {
-            using (var context = new SystemContext())
-            {
-                ///Creating properties for the Appointment
-                DateTime date = DateTime.Today;
-                DateTime meetingStart = DateTime.Now;
-                DateTime meetingEnd = DateTime.Now.AddHours(2);
-                //Create the Appointment itself 
-                Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1 };
-                //Save it to the database
-                context.Appointments.Add(testAppointment);
-                context.SaveChanges();
+            //Creating properties for the Appointment
+            DateTime date = DateTime.Today;
+            DateTime meetingStart = DateTime.Now;
+            DateTime meetingEnd = DateTime.Now.AddHours(2);
+           
+            //Create the Appointment itself 
+            Appointment testAppointment = new Appointment() { Date = date, StarTime = meetingStart, EndTime = meetingEnd, Category = "Sælgermøde", Description = "Test for at finde en appointment", Status = "Status", UserID = 1 };
 
-                //Delete it again
-                context.Appointments.Remove(testAppointment);
-                context.SaveChanges();
+            //Save it to the database
+            aCtr.InsertAppointment(testAppointment);
 
-                //Try to retrieve the deleted Appointment again, and assert it is null.
-                Appointment testAppointmentDB = context.Appointments.Where(x => x.Description == "Test for at finde en appointment").SingleOrDefault();
-                Assert.AreEqual(null, testAppointmentDB);
-            }
+            //Delete it again
+            aCtr.DeleteAppointment(testAppointment);
+
+            //Try to retrieve the deleted Appointment again, and assert it is null.
+            Appointment testAppointmentDB = aCtr.GetAppointment(date).SingleOrDefault();
+            Assert.AreEqual(null, testAppointmentDB);
         }
     }
 }
