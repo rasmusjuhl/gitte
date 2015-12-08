a8a722526d027bb125d2d004362a3d610730db50
SilasJeppe <Silas_Jeppe@hotmail.com>
2015-12-08 11:38:13 +0100

    Properties test done
    tests controlLayer instead of DBLayer

--- ControlLayerTest/propertyTest.cs
+++ ControlLayerTest/propertyTest.cs
@@ -5,82 +5,86 @@ using ModelLayer.DAL;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ControlLayer;
 
 namespace ControlLayerTest
 {
     [TestClass]
     public class propertyTest
     {
+        private PropertyController pCtr;
         [TestInitialize]
         public void Initialize()
         {
+            pCtr = new PropertyController();
         }
 
         [TestMethod]
-        public void CreateAndReadTest()
+        public void CreateTest()
         {
-            using (var context = new SystemContext())
-            {
-                //No properties does need to be created before the property itself 
-                //Create the property itself 
-                Property testProperty = new Property() { Address = "Sofiendalshej 60", ZipCode = "9000", Type = "Borg", Rooms = 4, Floors = 1, Price = 1000000, PropertySize = 1000, HouseSize = 100, ConstructionYear = 2000, SellerID = 1 };
-                //Save it to the database
-                context.Properties.Add(testProperty);
-                context.SaveChanges();
+            //No properties does need to be created before the property itself 
+            //Create the property itself 
+            Property testProperty = new Property() { Address = "Sofiendalshej 60", ZipCode = "9000", Type = "Borg", Rooms = 4, Floors = 1, Price = 1000000, PropertySize = 1000, HouseSize = 100, ConstructionYear = 2000, SellerID = 1 };
 
-                //Compare Properties. Maybe use conditions other than Adress?
-                Property testPropertyDB = context.Properties.Where(x => x.Address == "Sofiendalshej 60").Single();
-                Assert.AreEqual<Property>(testProperty, testPropertyDB);
-                context.Properties.Remove(testProperty);
-                context.SaveChanges();
-            }
+            //Save it to the database and get amount of properties in db before and after save
+            int CountDB = pCtr.GetAllProperties().ToList().Count;
+            pCtr.InsertProperty(testProperty);
+            int CountDBAfter = pCtr.GetAllProperties().ToList().Count;
 
+            //Compare Properties. Maybe use conditions other than Adress?
+            Assert.AreEqual<int>(CountDBAfter, CountDB + 1);
+            pCtr.DeleteProperty(testProperty);
+        }
+
+        [TestMethod]
+        public void ReadTest()
+        {
+            //No properties does need to be created before the property itself 
+            //Create the property itself 
+            Property testProperty = new Property() { Address = "Sofiendalshej 60", ZipCode = "9000", Type = "Borg", Rooms = 4, Floors = 1, Price = 1000000, PropertySize = 1000, HouseSize = 100, ConstructionYear = 2000, SellerID = 1 };
+
+            //Save it to the database and get amount of properties in db before and after save
+            pCtr.InsertProperty(testProperty);
+
+            //Get property from database and assert not null
+            Assert.IsNotNull(pCtr.GetPropertyByAddress(testProperty.Address));
+            pCtr.DeleteProperty(testProperty);
         }
 
         [TestMethod]
         public void UpdateTest()
         {
-            using (var context = new SystemContext())
-            {
-                //No properties does need to be created before the property itself 
-                //Create the property itself 
-                Property testProperty = new Property() { Address = "Sofiendalshej 60", ZipCode = "9000", Type = "Borg", Rooms = 4, Floors = 1, Price = 1000000, PropertySize = 1000, HouseSize = 100, ConstructionYear = 2000, SellerID = 1 };
-                //Save it to the database
-                context.Properties.Add(testProperty);
-                context.SaveChanges();
-                
-                //Change the property:
-                Property testPropertyDB = context.Properties.Where(x => x.Address == "Sofiendalshej 60").Single();
-                testProperty.Address = "Her";
-                context.SaveChanges();
-                
-                //Test the assertion
-                Assert.AreNotEqual(testPropertyDB.Address, testProperty.Address);
-                context.Properties.Remove(testProperty);
-                context.SaveChanges();
-            }
+            //No properties does need to be created before the property itself 
+            //Create the property itself 
+            Property testProperty = new Property() { Address = "Sofiendalshej 60", ZipCode = "9000", Type = "Borg", Rooms = 4, Floors = 1, Price = 1000000, PropertySize = 1000, HouseSize = 100, ConstructionYear = 2000, SellerID = 1 };
+
+            //Save it to the database
+            pCtr.InsertProperty(testProperty);
+
+            //Change the property:
+            pCtr.UpdateProperty(testProperty, "Her", "9000", "Borg", 4, 1, 1000000, 1000, 100, 2000);
+
+            //Test the assertion
+            Assert.IsNotNull(pCtr.GetPropertyByAddress("Her"));
+            pCtr.DeleteProperty(testProperty);
         }
 
         [TestMethod]
         public void DeleteTest()
         {
-            using (var context = new SystemContext())
-            {
-                //No properties does need to be created before the property itself 
-                //Create the property itself 
-                Property testProperty = new Property() { Address = "Sofiendalshej 60", ZipCode = "9000", Type = "Borg", Rooms = 4, Floors = 1, Price = 1000000, PropertySize = 1000, HouseSize = 100, ConstructionYear = 2000, SellerID = 1 };
-                //Save it to the database
-                context.Properties.Add(testProperty);
-                context.SaveChanges();
+            //No properties does need to be created before the property itself 
+            //Create the property itself 
+            Property testProperty = new Property() { Address = "Sofiendalshej 60", ZipCode = "9000", Type = "Borg", Rooms = 4, Floors = 1, Price = 1000000, PropertySize = 1000, HouseSize = 100, ConstructionYear = 2000, SellerID = 1 };
+            
+            //Save it to the database
+            pCtr.InsertProperty(testProperty);
 
-                //Delete it again
-                context.Properties.Remove(testProperty);
-                context.SaveChanges();
+            //Delete it again
+            pCtr.DeleteProperty(testProperty);
 
-                //Try to retrieve the deleted property again, and assert it is null.
-                Property testPropertyDB = context.Properties.Where(x => x.Address == "Sofiendalshej 60").SingleOrDefault();
-                Assert.AreEqual(null, testPropertyDB);
-            }
+            //Try to retrieve the deleted property again, and assert it is null.
+            Property testPropertyDB = pCtr.GetPropertyByAddress(testProperty.Address).SingleOrDefault();
+            Assert.IsNull(testPropertyDB);
         }
 
     }
