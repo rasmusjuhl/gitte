e3571b935b4cc35df80ba44abd8f5b60e0a01f16
Rasmus Juhl <rasmusjuhl@gmail.com>
2015-12-13 20:29:59 +0100

    Initial implementation of Appointment details viw

--- ASPServer/Controllers/AppointmentController.cs
+++ ASPServer/Controllers/AppointmentController.cs
@@ -16,5 +16,17 @@ namespace ASPServer.Controllers
             List<Appointment> appointments = iService.GetAllAppointments();
             return View(appointments);
         }
+
+        public ActionResult Details(int id)
+        {
+            /*Get all appointments again, and then find the one we need based on ID.
+             * We do this here instead of implementing GetBuyerById(int id) becase of limitations in EF
+            */
+            List<Appointment> appointments = iService.GetAllAppointments();
+            Appointment appointment = appointments.Find(listappointment => listappointment.Id == id);
+
+            //Make single appointment available to view
+            return View(appointment);
+        }
     }
 }
